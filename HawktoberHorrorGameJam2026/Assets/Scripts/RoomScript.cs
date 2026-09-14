61642adddf8fbf7df5763cb919a88502d37905e8
poppachoppapop <edward.aaron.ong@live.com>
2026-09-14 11:44:39 -0400

    Started RoomScript and new scene for testing

---
+++ HawktoberHorrorGameJam2026/Assets/Scripts/RoomScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class RoomScript : MonoBehaviour
+{
+    [SerializeField]
+    private bool IsLargeRoom = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
