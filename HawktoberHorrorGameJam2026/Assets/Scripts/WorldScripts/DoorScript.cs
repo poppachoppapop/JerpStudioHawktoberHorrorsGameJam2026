120278796f232ede43135f94a68e057515f67e2a
poppachoppapop <edward.aaron.ong@live.com>
2026-09-21 22:07:47 -0400

    Created DoorScript that sends player to connected room on collision with door

---
+++ HawktoberHorrorGameJam2026/Assets/Scripts/WorldScripts/DoorScript.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    [SerializeField]
+    private RoomScript connectedRoom;
+
+    [SerializeField]
+    private GameObject player;
+
+    [SerializeField]
+    private Camera mainCamera;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //     player = FindObjectOfType<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void EnterRoom()
+    {
+        // //setting camera position to new room
+        // mainCamera.transform.position = new Vector3(
+        // connectedRoom.transform.position.x,
+        // connectedRoom.transform.position.y,
+        // transform.position.z);
+
+        // player.transform.position = new Vector3()
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            mainCamera.transform.position = new Vector3(
+            connectedRoom.transform.position.x,
+            connectedRoom.transform.position.y,
+            -10);
+
+            player.transform.position = new Vector3(connectedRoom.transform.position.x,
+            connectedRoom.transform.position.y,
+            transform.position.z);
+
+            //Debug.Log("Wassup");
+        }
+    }
+}
